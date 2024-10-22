fb7a0cc6de39de090aecc9840157de001439d697
axel669 <haryuu.hanekata@gmail.com>
2024-10-21 22:22:11 -0700

    [change] reduce number of indexes per table to lower costs

--- service/sql/init.sql
+++ service/sql/init.sql
@@ -1,25 +1,34 @@
--- drop table if exists users;
+drop index if exists vault_find;
+drop table if exists vault;
 
--- create table users(
---     id integer primary key,
---     asuid text,
---     email text
--- );
--- create index
+drop index if exists api_key_find;
+drop table if exists api_keys;
 
+drop index if exists users_email_find;
+drop table if exists users;
+
+create table users(
+    asuid text not null primary key unique,
+    email text not null unique
+);
+create index users_email_find on users(email);
+
+drop index if exists api_key_find;
 drop table if exists api_keys;
 create table api_keys(
-    id integer primary key,
-    key text,
-    allowed text
+    users_asuid text not null,
+    key text not null unique,
+    desc text not null,
+    foreign key(users_asuid) references users(asuid)
 );
-create index key_find on api_keys(key);
+create index api_key_find on api_keys(users_asuid, key);
 
-drop table if exists vault;
 create table vault(
-    id integer primary key,
-    name text,
-    key text,
-    value text
+    asuid text not null primary key,
+    users_asuid text not null,
+    name text not null,
+    key text not null,
+    value text not null,
+    foreign key(users_asuid) references users(asuid)
 );
-create index vault_find on vault(name);
+create index vault_find on vault(users_asuid, asuid, name);
