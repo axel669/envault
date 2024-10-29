af6f10de1b30161265ff6f9b58918884c38bfbc7
axel669 <haryuu.hanekata@gmail.com>
2024-10-28 22:12:42 -0700

    [change] dont load keys and vars as part of users

--- service/src/routes/api/user.js
+++ service/src/routes/api/user.js
@@ -7,21 +7,21 @@ export const $get = async (c) => {
         return c.json(null)
     }
 
-    const items = await c.env.storage.prepare(
-        "select * from vault where vault.users_asuid = ?1"
-    ).bind(user.asuid).all()
+    // const items = await c.env.storage.prepare(
+    //     "select * from vault where vault.users_asuid = ?1"
+    // ).bind(user.asuid).all()
 
-    const apiKeys = await c.env.storage.prepare(
-        "select * from api_keys where api_keys.users_asuid = ?1"
-    ).bind(user.asuid).all()
-    for (const res of apiKeys.results) {
-        res.keyInfo = jwt.decode(res.key).payload
-    }
+    // const apiKeys = await c.env.storage.prepare(
+    //     "select * from api_keys where api_keys.users_asuid = ?1"
+    // ).bind(user.asuid).all()
+    // for (const res of apiKeys.results) {
+    //     res.keyInfo = jwt.decode(res.key).payload
+    // }
 
     return c.json({
         email: user.email,
         id: user.asuid,
-        items: items.results,
-        apiKeys: apiKeys.results,
+        // items: items.results,
+        // apiKeys: apiKeys.results,
     })
 }
--- site/src/app.svelte
+++ site/src/app.svelte
@@ -7,16 +7,29 @@
     import api from "#api"
 
     import Dashboard from "./comp/dashboard.svelte"
-    const user = api.user()
+    // const user = api.user()
+    const loadUser = async () => {
+        const [
+            user,
+            keys,
+            vars,
+        ] = await Promise.all([
+            api.user(),
+            api.keys.list(),
+            api.vault.list(),
+        ])
+
+        return [user.data, keys.data, vars.data]
+    }
     // window.http = http
 </script>
 
 <svelte:body use:wsx={{ "@@theme": "tron", "@@app": true }} />
 
-{#await user}
+{#await loadUser()}
     <Text>
         Loading
     </Text>
-{:then user}
-    <Dashboard user={user.data} />
+{:then [user, keys, vars]}
+    <Dashboard {user} {keys} {vars} />
 {/await}
