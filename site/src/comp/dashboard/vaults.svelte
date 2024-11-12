0796fb955d86397cb145a83f905d7439552a62d6
axel669 <haryuu.hanekata@gmail.com>
2024-11-11 23:06:22 -0800

    [add] working vault management controls

--- site/src/comp/dashboard/vaults.svelte
+++ site/src/comp/dashboard/vaults.svelte
@@ -1,9 +1,15 @@
 <script>
     import {
+        AsyncButton,
         Button,
         Input,
         Paper,
         Text,
+        EntryButton,
+        Icon,
+
+        Modal,
+        Confirm,
 
         Grid,
         Flex,
@@ -18,9 +24,6 @@
 
     export let vaults
 
-    vaults.forEach(
-        vault => vault.key = ""
-    )
     const hash = async (text) => {
         const bytes = await new Blob([text]).arrayBuffer()
         const hashBytes = await crypto.subtle.digest("SHA-512", bytes)
@@ -31,48 +34,84 @@
     }
 
     let currentVault = null
-    const loadVault = async (vault) => {
+    const save = async (vault) => {
         const vaultKey = await hash(vault.key)
-        const vaultInfo = await api.vault[vault.name]({
+        const res = await api.vault[vault.name.trim()]({
+            data: vault.content,
             headers: {
                 "vault-key": vaultKey,
             }
         })
-        const self = {
-            content: vaultInfo.data,
-            async save() {
-                const res = await api.vault[vault.name]({
-                    data: self.content,
-                    headers: {
-                        "vault-key": vaultKey,
-                    }
-                })
-                console.log(res)
-            }
+        if (vault.new === true) {
+            const res = await api.vaults()
+            vaults = res.data
+            return
         }
-        return self
     }
+    const createVault = () => ({
+        vault: {
+            new: true,
+            name: "",
+            key: "",
+            content: "",
+            save,
+        }
+    })
+    const openVault = handler$(
+        (vault) => ({
+            vault: {
+                new: false,
+                name: vault.name,
+                content: null,
+                key: "",
+                save,
+            }
+        })
+    )
+    let confirmModal = null
+    const removeVault = handler$(
+        async (vault) => {
+            const confirm = await confirmModal.show({
+                title: "Delete Vault",
+                message: `Delete ${vault.name}?`,
+                okText: "Continue"
+            })
+            if (confirm !== true) {
+                return
+            }
+            await api.vault[vault.name]({ method: "delete" })
+            const res = await api.vaults()
+            vaults = res.data
+        }
+    )
 </script>
 
-<Grid rows="1fr 1fr" p="0px" gap="0px" h="100%">
+<Modal component={Confirm} bind:this={confirmModal} />
+<Grid rows="min-content 1fr" p="0px" h="100%">
+    <EntryButton props={createVault} component={VaultEditor} color="@secondary"
+    outline>
+        + Create Vault
+    </EntryButton>
+
     <Flex p="0px">
         {#each vaults as vault}
-            <Paper card color="@primary">
-                <Text title>{vault.name}</Text>
-                <Input bind:value={vault.key}>
-                    <Text adorn slot="start" t.ws="pre" flex fl-center h="100%">
-                        Vault Key
-                    </Text>
+            <Paper card color="@primary" layout={Grid}
+            l-cols="48px 1fr 48px" l-p="2px">
+                <AsyncButton handler={removeVault(vault)} color="@danger" ground>
+                    <Icon name="trash" />
+                </AsyncButton>
 
-                    <Button slot="end" fill color="@secondary" disabled={vault.key.trim() === ""}
-                    on:click={() => currentVault = loadVault(vault)}>
-                        Load
-                    </Button>
-                </Input>
+                <Text>{vault.name}</Text>
+
+                <EntryButton props={openVault(vault)} component={VaultEditor}
+                fill color="@secondary" ground>
+                    <Icon name="pencil-square" />
+                </EntryButton>
             </Paper>
+        {:else}
+            <Text>
+                No vaults created
+            </Text>
         {/each}
     </Flex>
-    {#if currentVault !== null}
-        <Suspend component={VaultEditor} vault={currentVault} />
-    {/if}
 </Grid>
--- site/src/comp/dashboard/vaults/editor.svelte
+++ site/src/comp/dashboard/vaults/editor.svelte
@@ -1,12 +1,50 @@
 <script>
     import {
+        AsyncButton,
         Button,
+        Input,
+        Paper,
+        Screen,
+        Text,
+        Titlebar,
+
         Grid,
     } from "@axel669/zephyr"
 
     import { onMount } from "svelte"
 
+    import api from "#api"
+
     export let vault
+    export let close
+
+    const hash = async (text) => {
+        const bytes = await new Blob([text]).arrayBuffer()
+        const hashBytes = await crypto.subtle.digest("SHA-512", bytes)
+        return Array.from(
+            new Uint8Array(hashBytes),
+            (byte) => byte.toString(16).padStart(2, "0")
+        ).join("")
+    }
+
+    const cancel = () => close(null)
+    const open = async () => {
+        const vaultKey = await hash(vault.key)
+        const vaultInfo = await api.vault[vault.name]({
+            headers: {
+                "vault-key": vaultKey,
+            }
+        })
+        vault.content = vaultInfo.data
+        editor.session.setValue(vault.content)
+    }
+    const save = async () => {
+        await vault.save(vault)
+        if (vault.new === true) {
+            close(true)
+            return
+        }
+    }
 
     let editorDiv = null
     let editor = null
@@ -27,11 +65,48 @@
             () => vault.content = editor.session.getValue()
         )
     })
+
+    $: display = vault.content === null ? "none" : "block"
 </script>
 
-<Grid rows="min-content 1fr" p="0px" gap="0px">
-    <Button on:click={vault.save}>
-        Save
-    </Button>
-    <div ws-x="[h 100%]" bind:this={editorDiv} />
-</Grid>
+<Screen bg.c="rgba(0, 0, 0, 0.42)">
+    <Paper r="0px" layout={Grid}
+    l-rows="min-content min-content min-content 1fr">
+        <Titlebar slot="header" fill color="@primary">
+            <Text slot="title" title>
+                {vault.new ? "Create" : "Edit"} Vault
+            </Text>
+
+            <Button ground on:click={cancel} slot="action">
+                {#if vault.new === true}
+                    Cancel
+                {:else}
+                    Close
+                {/if}
+            </Button>
+        </Titlebar>
+
+        {#if vault.new === true}
+            <Input label="Vault Name" bind:value={vault.name} />
+        {:else}
+            <Text title p="8px">
+                Name: {vault.name}
+            </Text>
+        {/if}
+
+        <Input label="Vault Key" bind:value={vault.key} />
+
+        {#if vault.content === null}
+            <AsyncButton handler={open} fill color="@primary"
+            disabled={vault.key.length === 0}>
+                Open Vault
+            </AsyncButton>
+        {:else}
+            <AsyncButton handler={save} fill color="@secondary"
+            disabled={vault.key.length === 0 || vault.name.trim().length === 0}>
+                Save Vault
+            </AsyncButton>
+        {/if}
+        <div ws-x="[h 100%] [disp {display}]" bind:this={editorDiv} />
+    </Paper>
+</Screen>
